99e4f6b21efccd8ee69051111c94cad7dfdba2c0
Huron <charlie.walls97@gmail.com>
2024-06-29 11:23:56 -0500

    Add the HTML structure on the server

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from wsgiref.simple_server import make_server
 
+html="""
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Servidor python</title>
+    </head>
+    <body>
+        <h2> Hola mi nombre es calo</h2>
+    </body>
+</html>
+"""
+
 def aplication(env, start_response):
-    headers = [('Content-Type','text/plain')]
+    headers = [('Content-Type','text/html')]
     
     start_response('200 OK', headers)
     
-    return ['Hola desde mi primer servidor en python'.encode('utf-8')]
+    return [bytes(html,'utf-8')]
 
 server = make_server('localhost',3000,aplication)
 server.serve_forever()
