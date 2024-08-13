2949375bc0cfeaa8f726831e065439c6e7a03af6
Huron <charlie.walls97@gmail.com>
2024-08-13 11:20:04 -0500

    database conecct to fastapi

---
+++ database.py
@@ -0,0 +1,23 @@
+# database.py
+from databases import Database
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+
+# Crear una instancia de la base de datos
+DATABASE_URL = "postgresql://postgres:1234@localhost:5432/fastapi"
+database = Database(DATABASE_URL)
+
+# Crear un motor de SQLAlchemy
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Crear una base de datos
+metadata = MetaData()
+Base = declarative_base(metadata=metadata)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# crea una fábrica de sesiones configurada para trabajar con el
+#  motor de base de datos que hemos creado.
--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 from fastapi import FastAPI
+from database import database as connection
+from database import engine 
+from database import Base
 
-app = FastAPI(title="project", description="peliculas", version="1")
+
+
+
+app = FastAPI(title="project to rese;ar peliculas",
+               description="peliculas",
+               version="1")
+
+
+
+
+@app.on_event("startup")
+async def startup():
+    await connection.connect()
+    print("esta conectada")
+    # Crear las tablas en la base de datos
+    Base.metadata.create_all(bind=engine)
+
+@app.on_event("shutdown")
+async def shutdown():
+    # Desconectar de la base de datos
+    await connection.disconnect()
+    print('cerrando')
+
+
+
+
 
 
 @app.get("/")
 async def index():
-    return "Project of backend"
+    return "Project of backend conectado a la base de datos"
 
 
 @app.get("/about")
