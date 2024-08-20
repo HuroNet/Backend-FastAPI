ba9d22793df938049e7ff69458b1af13d2b00053
Huron <charlie.walls97@gmail.com>
2024-08-19 19:56:57 -0500

    format document check

--- schemas.py
+++ schemas.py
@@ -4,9 +4,11 @@ from pydantic import BaseModel
 class UserBaseModel(BaseModel):
     username: str
     password: str
+
+
 class MovieBaseModel(BaseModel):
     title: str
-    
+
+
 class UserReviewBaseModel(BaseModel):
     review_text: str
-    
