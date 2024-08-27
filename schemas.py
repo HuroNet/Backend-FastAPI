2a407b7c53e357c4b91a79d9843fa281a8ed6074
Huron <charlie.walls97@gmail.com>
2024-08-26 21:43:05 -0500

    drop the schemas to found the system

--- schemas.py
+++ schemas.py
@@ -7,12 +7,3 @@ class UserBaseModel(BaseModel):
     password: str
 
 
-class MovieBaseModel(BaseModel):
-    title: str
-    createal: TimeoutError
-
-
-class UserReviewBaseModel(BaseModel):
-    review_text: str
-    userid: str
-    movieid: str
